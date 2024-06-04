ae354c5e082c8f11706ab6a046945ec721668b55
IvanHSilva <ivanhenriquessilva@gmail.com>
2024-06-04 15:24:57 -0300

    Calculator Component + Imc Model

---
+++ BlazorPWAImc/Models/ImcModels.cs
@@ -0,0 +1,15 @@
+﻿namespace BlazorPWAImc.Models; 
+
+using System.ComponentModel.DataAnnotations;
+
+public class ImcModels {
+    [Required(ErrorMessage = "Altura obrigatória!")]
+    public double? Height { get; set; }
+
+    [Required(ErrorMessage = "Peso obrigatório!")]
+    public double? Weight { get; set; }
+
+    public DateTime CreatedAt { get; set; } = DateTime.Now;
+
+    public double Imc => (Weight ?? 1) / ((Height ?? 1) * (Height ?? 1));
+}
