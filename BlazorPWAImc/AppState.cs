08a096bc741b7d6a9af0049afdf7c8361f600e21
IvanHSilva <ivanhenriquessilva@gmail.com>
2024-06-06 12:01:06 -0300

    History + LocalStorage

---
+++ BlazorPWAImc/AppState.cs
@@ -0,0 +1,13 @@
+﻿using BlazorPWAImc.Models;
+
+namespace BlazorPWAImc; 
+
+public static class AppState {
+    public static List<ImcModels> History { get; set; } = [];
+
+    public static event EventHandler<EventArgs> OnChanged = null!;
+
+    public static void NotifyChanges(object? sender, EventArgs args) {
+        OnChanged.Invoke(sender, args);
+    }
+}
